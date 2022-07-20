c3c3785fa19d9586013d1018f9411c484e0395bd
Jake Williams <jakewilliams530@gmail.com>
2022-07-20 06:51:03 -0600

    Starting Files Added

---
+++ cv-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    
+  }
+
+  render() {
+    return <div>Hello World</div>
+  }
+}
+
+export default App;
